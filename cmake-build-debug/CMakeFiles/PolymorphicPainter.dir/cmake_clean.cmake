e4fb85d9f57048790d5c28ff0cb366c699355bcc
Nick-vdM <nicolaas.vdm1@gmail.com>
2019-09-08 19:35:37 +1000

    Merged into single files
    Took 1 minute

---
+++ cmake-build-debug/CMakeFiles/PolymorphicPainter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PolymorphicPainter.dir/2MyPolymorphicPainter.cpp.o"
+  "PolymorphicPainter.pdb"
+  "PolymorphicPainter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PolymorphicPainter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TemplatePainter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TemplatePainter.dir/1MyTemplatePainter.cpp.o"
+  "TemplatePainter.pdb"
+  "TemplatePainter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TemplatePainter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
